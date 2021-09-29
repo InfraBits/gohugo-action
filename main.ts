0716a4cf5331a8239d7e22ca1b05083c92ba0c77
Damian Zaremba <damian@damianzaremba.co.uk>
2021-09-29 18:58:53 +0100

    Use extended version for webp

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ import * as io from '@actions/io';
 
 async function run(): Promise<void> {
   const version: string = core.getInput('version');
-  const toolAssets: string = await tc.downloadTool(`https://github.com/gohugoio/hugo/releases/download/v${version}/hugo_${version}_Linux-64bit.tar.gz`);
+  const toolAssets: string = await tc.downloadTool(`https://github.com/gohugoio/hugo/releases/download/v${version}/hugo_extended_${version}_Linux-64bit.tar.gz`);
   const toolExtractedFolder: string = await tc.extractTar(toolAssets, "/tmp");
   await io.mv(`${toolExtractedFolder}/hugo`, "/usr/local/bin/hugo");
 }
